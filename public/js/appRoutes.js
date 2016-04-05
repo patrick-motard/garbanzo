3355b9eac74b6f6837c2912db599818e048fafa4
Mark Staebler <markstaebler@gmail.com>
2016-04-05 15:26:05 -0700

    auth post function setup

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -16,8 +16,20 @@ var module = angular.module("dashboard", ['ngRoute']);
                 });
         }]);
 
-    module.controller("RouteController", function($scope) {
+    module.controller("RouteController", function($scope, $http) {
+            // cannot set property name of undefined when uncommented
+            // trying to set default values
+            // $scope.user.name = "jim lahey";
+            // $scope.user.password = "iAmTheLiqour";
             $scope.login = function (){
-                console.log("submitted");
+                console.log($scope.user.name+"  "+$scope.user.password);
+                var data = {name: $scope.user.name , password: $scope.user.password};
+                var auth = $http.post("http://localhost:3000/authenticate",data)
+                    .then(
+                        function(){
+                            console.log("worked");
+                        }
+                    );
+                console.log("auth"+auth);
             }
     });
