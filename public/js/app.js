5a28d41e62e37b09e6f2d6d8836da2937dba66b2
Mark Staebler <markstaebler@gmail.com>
2016-04-12 17:01:23 -0700

    protected route added

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,30 @@ var app = angular.module('app', [
     'ui.router',
     'ngCookies',
 ])
+.run(function ($rootScope, $state, login) {
+  $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+    if (toState.authenticate && !login.state){
+      // User isn’t authenticated
+      $state.transitionTo("login");
+      event.preventDefault();
+    }
+  });
+})
 .config(['$stateProvider', '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
         'use-strict';
         $stateProvider.
-            state('/', {
+            state('login', {
                 url: '/',
                 templateUrl: 'js/login/login.html',
-                controller: 'LoginController'
+                controller: 'LoginController',
+                authenticate: false
             }).
-            state('/home', {
+            state('home', {
                 url:'/home',
                 templateUrl: 'js/home/home.html',
-                controller: 'HomeController'
+                controller: 'HomeController',
+                authenticate: true
             });
         $urlRouterProvider.otherwise('/');
 }]);
