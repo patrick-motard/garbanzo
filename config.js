3172ee352ef2ba8f32dd172e72cae85a378a4c6e
Patrick Motard <patrick.motard@johnsonfit.com>
2016-05-25 22:56:24 -0500

    removed old config stuff

--- config.js
+++ config.js
@@ -7,23 +7,13 @@ module.exports = () => {
             password: p.GARBANZO_PASSWORD,
             user: p.GARBANZO_USER
         },
-        secret: process.env.SECRET,
+        secret: p.SECRET,
         redditAuth: {
-            access_token
+            access_token: p.REDDIT_ACCESS_TOKEN,
+            token_type: 'bearer',
+            expires_in: 3600,
+            refresh_token: p.REDDIT_REFRESH_TOKEN,
+            scope: 'account edit history identity'
         }
-    }
-    "secret": "there's always money in the banana stand",
-    "redditAuth": {
-        "access_token": "45422354-IJ9yL1kqs9qXhbqAD6a1r5O11Jg",
-        "token_type": "bearer",
-        "expires_in": 3600,
-        "refresh_token": "45422354-MxQgNTuo0vqWEiYwBciowvb3a4g",
-        "scope": "account edit history identity"
-    },
-    "mysql_credentials_local": {
-        "host":"127.0.0.1:3306",
-        "user":"root",
-        "password":"",
-        "database":"garbanzo"
     }
 }
